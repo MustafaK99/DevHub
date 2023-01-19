97bf12fe23bbe966cef1099985d840832dafcfd3
MustafaK99 <rmk799@outlook.com>
2023-01-19 17:56:02 +0000

    button filter logic

--- client/src/pages/Board/Board.js
+++ client/src/pages/Board/Board.js
@@ -21,20 +21,17 @@ const Board = () => {
     const [dragEl, setDragEl] = useState(null);
     const [statusIcons, setStatusIcons] = useState([]);
 
-    const top100Films = [
-        { label: 'The Shawshank Redemption', year: 1994 },
-        { label: 'The Godfather', year: 1972 },
-        { label: 'The Godfather: Part II', year: 1974 },
-        { label: 'The Dark Knight', year: 2008 },
-        { label: '12 Angry Men', year: 1957 },
-        { label: "Schindler's List", year: 1993 },
-        { label: 'Pulp Fiction', year: 1994 },
-        {
-            label: 'The Lord of the Rings: The Return of the King',
-            year: 2003,
-        }
-    ]
 
+    const filter = (button) => {
+
+        if (button === 'All') {
+            setMenuItem(items);
+            return;
+        }
+
+        const filteredData = items.filter(item => item.category === button);
+        setMenuItem(filteredData)
+    }
 
 
 
