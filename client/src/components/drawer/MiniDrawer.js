b4fdee131fdd2891a188435d716c808cd8cc4b65
MustafaK99 <rmk799@outlook.com>
2023-01-26 19:03:46 +0000

    added new stuff to drawer and productbacklog model might delete tho

--- client/src/components/drawer/MiniDrawer.js
+++ client/src/components/drawer/MiniDrawer.js
@@ -1,25 +1,22 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import AddIcon from '@mui/icons-material/Add';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import QueueIcon from '@mui/icons-material/Queue';
+import SettingsIcon from '@mui/icons-material/Settings';
+import MuiAppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import MuiDrawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import SettingsIcon from '@mui/icons-material/Settings';
-import AddIcon from '@mui/icons-material/Add';
-import QueueIcon from '@mui/icons-material/Queue';
-import DashboardIcon from '@mui/icons-material/Dashboard';
+import { styled, useTheme } from '@mui/material/styles';
+import * as React from 'react';
 import Window from "./Window";
 
 
@@ -76,7 +73,7 @@ const AppBar = styled(MuiAppBar, {
 
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
 
-  
+
   ({ theme, open }) => ({
     width: drawerWidth,
     flexShrink: 0,
@@ -99,7 +96,7 @@ export default function MiniDrawer() {
   const [show, setShow] = React.useState(false);
 
 
- 
+
 
   const handleDrawerOpen = () => {
     setOpen(!open);
@@ -112,59 +109,59 @@ export default function MiniDrawer() {
 
   return (
     <Box sx={{ display: 'flex' }}>
-      <Drawer variant="permanent" open={open} PaperProps={{ sx: {backgroundColor: "#24292f" , color: "white"}}}>
+      <Drawer variant="permanent" open={open} PaperProps={{ sx: { backgroundColor: "#24292f", color: "white" } }}>
 
         <DrawerHeader>
-          <IconButton onClick={handleDrawerOpen} sx={{ color: 'white',}}>
+          <IconButton onClick={handleDrawerOpen} sx={{ color: 'white', }}>
             {open === true ? <ChevronRightIcon /> : <ChevronLeftIcon />}
 
           </IconButton>
         </DrawerHeader>
-        <Divider sx={{borderColor: 'white'}}/>
+        <Divider sx={{ borderColor: 'white' }} />
         <List>
-          <ListItem disablePadding sx={{display: 'block'}}>
+          <ListItem disablePadding sx={{ display: 'block' }}>
             <ListItemButton
-                onClick={onOpen}
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}              
+              onClick={onOpen}
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
             >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                    color: 'white'
-                  }}
-                >
-                    <AddIcon />
-                </ListItemIcon>
-                <ListItemText primary='New Board' sx={{ opacity: open ? 1 : 0 }} />
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                  color: 'white'
+                }}
+              >
+                <AddIcon />
+              </ListItemIcon>
+              <ListItemText primary='New Board' sx={{ opacity: open ? 1 : 0 }} />
             </ListItemButton>
 
           </ListItem>
 
-        <ListItem disablePadding sx={{ display: 'block' }}>  
-          <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}              
+          <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
             >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                    color: 'white',
-                  }}
-                >
-                    <DashboardIcon />
-                </ListItemIcon>
-                <ListItemText primary='Active Sprints' sx={{ opacity: open ? 1 : 0 }} />
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                  color: 'white',
+                }}
+              >
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary='Active Sprints' sx={{ opacity: open ? 1 : 0 }} />
             </ListItemButton>
           </ListItem>
 
@@ -191,17 +188,17 @@ export default function MiniDrawer() {
                 </ListItemIcon>
                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
-                
+
               <Window
                 onClose={onClose}
                 show={show}
-            />
+              />
 
 
             </ListItem>
           ))}
         </List>
-        <Divider sx={{borderColor: 'white'}}/>
+        <Divider sx={{ borderColor: 'white' }} />
         <List>
           {['New Project', 'Project Settings'].map((text, index) => (
             <ListItem key={text} disablePadding sx={{ display: 'block' }}>
@@ -220,15 +217,37 @@ export default function MiniDrawer() {
                     color: 'white',
                   }}
                 >
-                  {index === 0  && <AddIcon/>}
-                  {index === 1 && <SettingsIcon/> }
+                  {index === 0 && <AddIcon />}
+                  {index === 1 && <SettingsIcon />}
 
                 </ListItemIcon>
                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
             </ListItem>
+
           ))}
         </List>
+        <Divider sx={{ borderColor: 'white' }} />
+        <List>
+          <ListItem>
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: open ? 3 : 'auto',
+                justifyContent: 'center',
+                color: 'white',
+              }}
+            >
+              {<AccountTreeIcon />}
+
+            </ListItemIcon>
+            <ListItemText primary={"Active Projects"} sx={{ opacity: open ? 1 : 0 }} />
+
+          </ListItem>
+        </List>
+
+
+
       </Drawer>
     </Box>
   );
