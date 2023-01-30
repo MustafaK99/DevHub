4b576f6fb70249fd8a5429c83661b649e0d5d917
MustafaK99 <rmk799@outlook.com>
2023-01-30 19:23:37 +0000

    can get all projects for projects page

---
+++ client/src/components/projectItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ProjectItem({ project }) {
+    return (
+        <div>
+            <h1>{project.name}</h1>
+
+        </div>
+    )
+}
+
+export default ProjectItem
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import ProjectItem from "../components/projectItem";
+import { getProjects, reset } from '../features/projects/projectSlice';
 
 const Projects = () => {
+
+    const dispatch = useDispatch()
+
+    const { projects, isLoading, isError, message } = useSelector((state) => state.projects)
+
+    useEffect(() => {
+        if (isError) {
+            console.log(message)
+        }
+
+        dispatch(getProjects())
+
+        return () => {
+            dispatch(reset())
+        }
+    }, [isError, message, dispatch])
+
     return (
-        <h1>Active Projects</h1>
+
+        <>
+            <h1>Active Projects</h1>
+
+            <section>
+                {projects.length > 0 ? (
+                    <div>
+                        {projects.map((project) => (
+                            <ProjectItem key={project._id} project={project} />
+                        ))}
+                    </div>
+                ) : (<h3>No Active Projects Currently </h3>)}
+
+            </section>
+        </>
+
     )
 
 }
