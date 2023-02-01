1fe456155650253b5dac5b6060936d49ff57b20e
MustafaK99 <rmk799@outlook.com>
2023-02-01 18:37:34 +0000

    deleted database stuff and looked at css

--- client/src/components/projectItem/projectItem.js
+++ client/src/components/projectItem/projectItem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './projectItem.css';
 
+
 function ProjectItem({ project }) {
+
+    const handleClick = () => {
+        console.log('open project')
+    }
+
     return (
 
-        <div className='projectItem'>
+        <div className='projectItem' onClick={handleClick}>
             <h1>{project.name}</h1>
+
+            <p>{project.description}</p>
 
         </div>
 
