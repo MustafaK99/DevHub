9b5d2577597132526480a9d9e1e68a085c8a5e86
MustafaK99 <rmk799@outlook.com>
2023-02-04 19:12:49 +0000

    works well need to think about how to make content changeable

--- client/src/components/projectItem/projectItem.js
+++ client/src/components/projectItem/projectItem.js
@@ -7,7 +7,13 @@ function ProjectItem({ project }) {
     const navigate = useNavigate();
 
     const handleClick = () => {
-        navigate("/project", { state: { id: project._id, name: project.name } })
+        navigate("/project", {
+            state: {
+                id: project._id, name: project.name, description: project.description,
+                start_time: project.start_time, end_time: project.end_time, organisation: project.organisation,
+                collaborators: project.collaborators, epics: project.epics, sprints: project.sprints, backlog: project.backlog
+            }
+        })
     }
 
     return (
--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -10,6 +10,13 @@ const Project = () => {
         <div>
             <h1>Project id: {location.state.id}</h1>
             <h1>Project Name: {location.state.name}</h1>
+            {location.state.description != null ? <h1>Project Description: {location.state.description}</h1> : <h1>No description</h1>}
+            {location.state.start_time != null ? <h1>location.state.start_time</h1> : <h1>No start_time</h1>}
+            {location.state.end_time != null ? <h1>location.state.end_time</h1> : <h1>No end_time</h1>}
+            {location.state.organisation != null ? <h1>{location.state.organisation}</h1> : <h1>no organisation</h1>}
+
+
+
         </div>
     )
 }
