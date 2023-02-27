ea586a481ec3a8fd334a0fdb2a7804724d5f7489
MustafaK99 <rmk799@outlook.com>
2023-02-27 19:05:37 +0000

    updated mini-drawer

--- client/src/components/drawer/MiniDrawer.js
+++ client/src/components/drawer/MiniDrawer.js
@@ -1,94 +1,91 @@
-import AccountTreeIcon from '@mui/icons-material/AccountTree';
-import AddIcon from '@mui/icons-material/Add';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import QueueIcon from '@mui/icons-material/Queue';
-import SettingsIcon from '@mui/icons-material/Settings';
-import MuiAppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import MuiDrawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { styled, useTheme } from '@mui/material/styles';
-import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import AddIcon from "@mui/icons-material/Add";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import QueueIcon from "@mui/icons-material/Queue";
+import SettingsIcon from "@mui/icons-material/Settings";
+import MuiAppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import MuiDrawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { styled, useTheme } from "@mui/material/styles";
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
 import Window from "./Window";
 
 const drawerWidth = 240;
 
-
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-
-
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
   }),
-);
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
 export default function MiniDrawer() {
   const theme = useTheme();
@@ -96,12 +93,11 @@ export default function MiniDrawer() {
   const [open, setOpen] = React.useState(false);
   const [show, setShow] = React.useState(false);
 
-
   const navigate = useNavigate();
 
   const handleDrawerOpen = () => {
     setOpen(!open);
-    console.log(open)
+    console.log(open);
   };
 
   const onOpen = () => setShow(true);
@@ -109,80 +105,86 @@ export default function MiniDrawer() {
   const onClose = () => setShow(false);
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Drawer variant="permanent" open={open} PaperProps={{ sx: { backgroundColor: "#24292f", color: "white" } }}>
-
+    <Box sx={{ display: "flex" }}>
+      <Drawer
+        variant="permanent"
+        open={open}
+        PaperProps={{ sx: { backgroundColor: "#24292f", color: "white" } }}
+      >
         <DrawerHeader>
-          <IconButton onClick={handleDrawerOpen} sx={{ color: 'white', }}>
+          <IconButton onClick={handleDrawerOpen} sx={{ color: "white" }}>
             {open === true ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-
           </IconButton>
         </DrawerHeader>
-        <Divider sx={{ borderColor: 'white' }} />
+        <Divider sx={{ borderColor: "white" }} />
         <List>
-          <ListItem disablePadding sx={{ display: 'block' }}>
+          <ListItem disablePadding sx={{ display: "block" }}>
             <ListItemButton
               onClick={onOpen}
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
               }}
             >
               <ListItemIcon
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
-                  color: 'white'
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                  color: "white",
                 }}
               >
                 <AddIcon />
               </ListItemIcon>
-              <ListItemText primary='New Board' sx={{ opacity: open ? 1 : 0 }} />
+              <ListItemText
+                primary="New Board"
+                sx={{ opacity: open ? 1 : 0 }}
+              />
             </ListItemButton>
-
           </ListItem>
 
-          <ListItem disablePadding sx={{ display: 'block' }}>
+          <ListItem disablePadding sx={{ display: "block" }}>
             <ListItemButton
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
               }}
             >
               <ListItemIcon
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
-                  color: 'white',
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                  color: "white",
                 }}
               >
                 <DashboardIcon />
               </ListItemIcon>
-              <ListItemText primary='Active Sprints' sx={{ opacity: open ? 1 : 0 }} />
+              <ListItemText
+                primary="Active Sprints"
+                sx={{ opacity: open ? 1 : 0 }}
+              />
             </ListItemButton>
           </ListItem>
 
-          {['Backlog'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+          {["Backlog"].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: "block" }}>
               <ListItemButton
                 onClick={onOpen}
                 sx={{
                   minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
+                  justifyContent: open ? "initial" : "center",
                   px: 2.5,
                 }}
               >
                 <ListItemIcon
                   sx={{
                     minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                    color: 'white',
-
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                    color: "white",
                   }}
                 >
                   {index === 0 && <QueueIcon />}
@@ -190,76 +192,69 @@ export default function MiniDrawer() {
                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
 
-              <Window
-                onClose={onClose}
-                show={show}
-              />
-
-
+              <Window onClose={onClose} show={show} />
             </ListItem>
           ))}
         </List>
-        <Divider sx={{ borderColor: 'white' }} />
+        <Divider sx={{ borderColor: "white" }} />
         <List>
-          {['New Epic', 'Epics', 'New Feature', 'Features', 'Project Settings'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
+          {["New Epic", "Epics", "New Feature", "Features"].map(
+            (text, index) => (
+              <ListItem key={text} disablePadding sx={{ display: "block" }}>
+                <ListItemButton
                   sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                    color: 'white',
+                    minHeight: 48,
+                    justifyContent: open ? "initial" : "center",
+                    px: 2.5,
                   }}
                 >
-                  {index === 0 && <AddIcon />}
-                  {index === 1 && <DashboardIcon />}
-                  {index === 2 && <AddIcon />}
-                  {index === 3 && <DashboardIcon />}
-                  {index === 4 && <SettingsIcon />}
-
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-
-          ))}
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : "auto",
+                      justifyContent: "center",
+                      color: "white",
+                    }}
+                  >
+                    {index === 0 && <AddIcon />}
+                    {index === 1 && <DashboardIcon />}
+                    {index === 2 && <AddIcon />}
+                    {index === 3 && <DashboardIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </ListItem>
+            )
+          )}
         </List>
-        <Divider sx={{ borderColor: 'white' }} />
+        <Divider sx={{ borderColor: "white" }} />
         <List>
-          <ListItem disablePadding sx={{ display: 'block' }}>
+          <ListItem disablePadding sx={{ display: "block" }}>
             <ListItemButton
-              onClick={() => navigate('/projects')}
+              onClick={() => navigate("/projects")}
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
               }}
             >
               <ListItemIcon
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
-                  color: 'white'
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                  color: "white",
                 }}
               >
                 <AccountTreeIcon />
               </ListItemIcon>
-              <ListItemText primary='Active Projects' sx={{ opacity: open ? 1 : 0 }} />
+              <ListItemText
+                primary="Active Projects"
+                sx={{ opacity: open ? 1 : 0 }}
+              />
             </ListItemButton>
-
           </ListItem>
         </List>
-
-
-
       </Drawer>
     </Box>
   );
