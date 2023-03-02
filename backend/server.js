969d88a95f754e365cbdbacfbaf2340edcc8be36
MustafaK99 <rmk799@outlook.com>
2023-03-02 18:05:14 +0000

    added epic routes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
-const express = require("express")
-const colors = require('colors')
-const dotenv = require("dotenv").config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
-const port = process.env.PORT || 5000
-const cors = require('cors')
-connectDB()
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-app.use('/api/tickets', require('./routes/ticketRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/projects', require('./routes/projectRoutes'))
+app.use("/api/tickets", require("./routes/ticketRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/projects", require("./routes/projectRoutes"));
+app.use("/api/epics", require("./routes/epicRoutes"));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`server is listening on ${port}`))
+app.listen(port, () => console.log(`server is listening on ${port}`));
