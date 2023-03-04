f5c93db6ba7eb5b2f63b48fb61f9f5c4576dad6d
MustafaK99 <rmk799@outlook.com>
2023-03-04 18:03:21 +0000

    updated the form a little

--- backend/models/featureModel.js
+++ backend/models/featureModel.js
@@ -1,38 +1,60 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const featureSchema = mongoose.Schema({
-
+const featureSchema = mongoose.Schema(
+  {
     created_by_user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
 
     epic: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Epic'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Epic",
     },
 
     status: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     content: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    tickets: [{
-        type: mongoose.Schema.Types.ObjectId,
-        required: false,
-        ref: 'Ticket'
-    }]
-}, {
-    timestamps: true
-})
+    tickets: [
+      {
+        id: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: false,
+          ref: "Ticket",
+        },
+        status: {
+          type: String,
+          required: true,
+        },
+        title: {
+          type: String,
+          required: true,
+        },
+        priority: {
+          type: String,
+          required: true,
+        },
+        estimate: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Feature', featureSchema)
+module.exports = mongoose.model("Feature", featureSchema);
