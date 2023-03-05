af9dbcbc63bc8917a22744f657bf95027d3f9ae6
MustafaK99 <rmk799@outlook.com>
2023-03-05 18:22:23 +0000

    conditioanl rendering for epics

--- client/src/pages/Epics/Epics.js
+++ client/src/pages/Epics/Epics.js
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import "./Epics.css";
 
 const Epics = () => {
+  const { activeProject } = useSelector((state) => state.projects);
+
   return (
     <>
-      <div className="container-xml">
-        <div className="table-responsive">
-          <div className="table-wrapper">display list of epics</div>
+      {activeProject ? (
+        <div className="container-xml">
+          <div className="table-responsive">
+            <div className="table-wrapper">display list of epics</div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <p>Please select a project</p>
+        </div>
+      )}
     </>
   );
 };
