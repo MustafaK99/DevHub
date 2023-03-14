ebfae4143c6ed0a27ade054d152b278bfc500ed0
MustafaK99 <rmk799@outlook.com>
2023-03-14 17:32:12 +0000

    started the slice/service for epics

---
+++ client/src/features/epics/epicService.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+
+const API_URL = "/api/projects/";
+
+//create new project
+
+const createEpic = async (epicData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.post(API_URL, epicData, config);
+
+  return response.data;
+};
+
+//delete project
+
+const deleteEpic = async (epicId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.delete(API_URL + epicId, config);
+
+  return response.data;
+};
+
+// get all projects
+const getEpics = async (id, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL + id, config);
+
+  return response.data;
+};
+
+//update project
+
+const updateEpic = async (epicData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.put(
+    API_URL + epicData.epicId,
+    epicData.epicData,
+    config
+  );
+
+  return response.data;
+};
+
+const epicService = {
+  createEpic,
+  getEpics,
+  deleteEpic,
+  updateEpic,
+};
+
+export default epicService;
---
+++ client/src/features/epics/epicSlice.js
@@ -0,0 +1,153 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import epicService from "./epicService";
+
+const initialState = {
+  epics: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+// create new project
+
+export const createEpic = createAsyncThunk(
+  "epics/create",
+  async (epicData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await epicService.createProject(epicData, token);
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.respone.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// delete  project
+
+export const deleteEpic = createAsyncThunk(
+  "epics/delete",
+  async (id, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await epicService.deleteEpic(id, token);
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.respone.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+//Get epics for given Project
+
+export const getEpics = createAsyncThunk(
+  "epics/getAll",
+  async (id, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await epicService.getEpics(id, token);
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.respone.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+//Update Project
+export const updateEpic = createAsyncThunk(
+  "projects/update",
+  async (Data, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await epicService.updateEpic(Data, token);
+    } catch (error) {
+      const message =
+        (error.response && error.response.data && error.respone.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const projectSlice = createSlice({
+  name: "epic",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createEpic.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createEpic.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.epics.push(action.payload);
+      })
+      .addCase(createEpic.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getEpics.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getEpics.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.epics = action.payload;
+      })
+      .addCase(getEpics.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(deleteEpic.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteEpic.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.epics = state.epics.filter(
+          (epic) => epic._id !== action.payload.id
+        );
+      })
+      .addCase(deleteEpic.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(updateEpic.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateEpic.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.epics = [...state.epics, action.payload];
+      })
+      .addCase(updateEpic.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
+
+export const { reset } = projectSlice.actions;
+export default projectSlice.reducer;
