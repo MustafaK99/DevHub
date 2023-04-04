ef44066d14fee83a4ef65a24ac83096d3d1040dd
MustafaK99 <rmk799@outlook.com>
2023-04-04 19:22:21 +0100

    made issue service

---
+++ client/src/features/issues/issueService.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+
+const API_URL = "/api/epics/";
+
+//create new issue
+
+const createIssue = async (issueData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.post(API_URL, issueData, config);
+
+  return response.data;
+};
+
+//delete issue
+
+const deleteIssue = async (issueId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.delete(API_URL + issueId, config);
+
+  return response.data;
+};
+
+// get all issues
+const getIssues = async (id, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL + id, config);
+
+  return response.data;
+};
+
+//update issue
+
+const updateIssue = async (issueData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.put(
+    API_URL + issueData.issueId,
+    issueData.issueData,
+    config
+  );
+
+  return response.data;
+};
+
+const issueService = {
+  createIssue,
+  getIssues,
+  deleteIssue,
+  updateEpic,
+};
+
+export default issueService;
